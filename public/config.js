f15cceec54fc3c693bac82e85552ddc1e2712910
orderly-guoqing <guoqing@orderly.network>
2026-07-10 17:04:26 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://168.144.157.73.nip.io",
+  "VITE_SEO_SITE_URL": "https://ssrf-test-orderly.168.144.157.73.nip.io",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
